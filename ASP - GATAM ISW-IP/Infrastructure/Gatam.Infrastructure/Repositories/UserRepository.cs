495e62a1ba74aad42634b69261ce2480f71192fd
Haitam <Haitam.baqloul@hotmail.com>
2024-10-02 22:20:28 +0200

    add both interfaces for unit of work and user

---
+++ ASP - GATAM ISW-IP/Infrastructure/Gatam.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Gatam.Application.Interfaces;
+using Gatam.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Infrastructure.Repositories
+{
+    public class UserRepository :IUserRepository
+    {
+        private readonly List<User> _users = new List<User>
+        {
+            new User { Name = "John Doe", Email = "john.doe@example.com", Password = "hashedpassword1" },
+            new User { Name = "Jane Doe", Email = "jane.doe@example.com", Password = "hashedpassword2" }
+        };
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await Task.FromResult(_users);
+        }
+    }
+}
---
+++ ASP - GATAM ISW-IP/Infrastructure/Gatam.Infrastructure/UOW/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Gatam.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Infrastructure.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UnitOfWork(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IUserRepository UserRepository => _userRepository;
+
+
+
+        public async Task commit()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
