a0bc3cea12da8793d84e06fd625b463c9b37de1e
Haitam <Haitam.baqloul@hotmail.com>
2024-10-02 22:20:52 +0200

    add webAPI controller to get the data

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Gatam.Application.CQRS;
+using Gatam.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Application.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<User>> GetAllUsers();
+    }
+}
