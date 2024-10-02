6e7723243c8101af06b523e7a5d37de16be6d75d
Haitam <Haitam.baqloul@hotmail.com>
2024-10-02 22:19:21 +0200

    add a registrator to Infrastructure

---
+++ ASP - GATAM ISW-IP/Infrastructure/Gatam.Infrastructure/Extensions/Registrator.cs
@@ -0,0 +1,22 @@
+﻿using Gatam.Application.Interfaces;
+using Gatam.Infrastructure.Repositories;
+using Gatam.Infrastructure.UOW;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Infrastructure.Extensions
+{
+    public static class Registrator
+    {
+        public static IServiceCollection RegisterInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
