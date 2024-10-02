50a21919a44c1f219e862e4100384f7ab14c7927
Haitam <Haitam.baqloul@hotmail.com>
2024-10-02 22:20:39 +0200

    add mapping for the user

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Mapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gatam.Application.CQRS;
+using AutoMapper;
+using Gatam.Domain;
+namespace Gatam.Application
+{
+    public class Mapping:Profile
+    {
+        public Mapping()
+        {
+            CreateMap<User, UserDTO>();
+
+        }
+    }
+}
