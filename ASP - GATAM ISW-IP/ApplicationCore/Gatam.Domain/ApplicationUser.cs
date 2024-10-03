8659cf2b2174a972ec335118d422728f6bc1d864
Haitam <Haitam.baqloul@student.ap.be>
2024-10-03 17:10:45 +0200

    feat: change entity classe to applicationuser

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/ApplicationUser.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Gatam.Domain
+{
+
+    public enum ApplicationUserRoles
+    {
+        ADMIN,
+        MENTOR,
+        STUDENT
+    }
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUserRoles _role { get; set; }
+
+
+    }
+
+}
