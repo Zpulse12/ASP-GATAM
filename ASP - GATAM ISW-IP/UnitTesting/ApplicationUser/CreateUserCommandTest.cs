db69e1e48d84182e76427071bc00ccf18a431bda
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-10-06 17:41:28 +0200

    feat: add testing

---
+++ ASP - GATAM ISW-IP/UnitTesting/ApplicationUser/CreateUserCommandTest.cs
@@ -0,0 +1,24 @@
+﻿using Gatam.Application.Interfaces;
+using Gatam.Infrastructure.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ApplicationUser
+{
+    [TestClass]
+    internal class CreateUserCommandTest
+    {
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+        private Mock<UserRepository> _userRepositoryMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+    }
+}
