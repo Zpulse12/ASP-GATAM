e1d4739e645fd073e3814a2cb2631ab802172844
Haitam <Haitam.baqloul@student.ap.be>
2024-10-07 17:41:35 +0200

    feat: made some unittesting for the get all user controller and GetallQuery

---
+++ ASP - GATAM ISW-IP/UnitTesting/UnitTesting/UnitTesting/ControllerTest/GetAllUserController.cs
@@ -0,0 +1,47 @@
+﻿using Gatam.Application.CQRS;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ControllerTest
+{
+    [TestClass]
+    public class GetAllUserControllerTest
+    {
+        private Mock<IMediator>? mediator;
+        private UserController? controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mediator = new Mock<IMediator>();
+            controller = new UserController(mediator.Object);
+        }
+
+        [TestMethod]
+        public async Task GetUsers_ReturnsOkResult_WithUsers()
+        {
+            var users = new List<UserDTO>
+            {
+                new UserDTO { Username = "user1", Email = "user1@example.com" },
+                new UserDTO { Username = "user2", Email = "user2@example.com" }
+            };
+
+            mediator.Setup(m => m.Send(It.IsAny<GetAllUsersQuery>(), default))
+                          .ReturnsAsync(users);
+
+            var result = await controller.GetUsers();
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(users, okResult.Value);
+        }
+    }
+}
