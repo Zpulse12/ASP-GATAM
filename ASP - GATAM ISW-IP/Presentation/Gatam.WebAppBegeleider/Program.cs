4d2227df8e4a9556b18950dff1ecda2e6dbeee17
lauren onya <laurenonya@hotmail.com>
2024-10-11 09:46:50 +0200

    feat: add auth0

--- ASP - GATAM ISW-IP/Presentation/Gatam.WebAppBegeleider/Program.cs
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAppBegeleider/Program.cs
@@ -1,14 +1,32 @@
 using Gatam.WebAppBegeleider.Components;
+<<<<<<< Updated upstream
+=======
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;          
+using Microsoft.AspNetCore.Authentication.Cookies;
+>>>>>>> Stashed changes
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services
+            .AddAuth0WebAppAuthentication(options => {
+                options.Domain = builder.Configuration["Auth0:Domain"];
+                options.ClientId = builder.Configuration["Auth0:ClientId"];
+            });
 
         // Voeg services toe aan de container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+<<<<<<< Updated upstream
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
@@ -25,6 +43,19 @@ internal class Program
             await next.Invoke();
         });
 
+=======
+        
+
+        string baseURI = "http://webapi:8080/";
+        #if DEBUG
+             baseURI = "http://localhost:5000";
+        #endif
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseURI) });
+        
+
+        var app = builder.Build();
+
+>>>>>>> Stashed changes
         // Configureer de HTTP-request-pijplijn.
         if (!app.Environment.IsDevelopment())
         {
@@ -34,9 +65,24 @@ internal class Program
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
+<<<<<<< Updated upstream
         app.UseRouting();
         app.UseAuthentication(); // Zorg ervoor dat authenticatie is ingesteld
         app.UseAuthorization();
+=======
+        app.UseAntiforgery();
+        app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                    .WithRedirectUri(returnUrl)
+                    .Build();
+
+            await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        });
+        
+        //app.UseAuthentication(); // Zorg ervoor dat authenticatie is ingesteld
+        //app.UseAuthorization();
+>>>>>>> Stashed changes
 
         app.MapRazorPages(); // Registreer de Razor-pagina's
         app.MapControllers(); // Indien nodig voor API controllers
