b8f7a699a560205238bee8abb533c6d6e8b23ee8
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-10-23 14:09:11 +0200

    fix: remove tailwind

--- ASP - GATAM ISW-IP/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./**/*.{razor, html}', './Presentation/Gatam.WebAppBegeleider/Components/**/*.{razor, html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
