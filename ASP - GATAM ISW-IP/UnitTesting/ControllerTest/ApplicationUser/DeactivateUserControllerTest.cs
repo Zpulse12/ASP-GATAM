630493174854d8f7f444835705ff25e80b2951d9
Haitam <haitam.baqloul@student.ap.be>
2024-10-24 23:51:32 +0200

    fix:: error fix

--- ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/DeactivateUserControllerTest.cs
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/DeactivateUserControllerTest.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gatam.Application.CQRS.User;
 using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace UnitTesting.ControllerTest.ApplicationUser
@@ -40,7 +41,7 @@ namespace UnitTesting.ControllerTest.ApplicationUser
                 }
             };
 
-            mediator.Setup(m => m.Send(It.IsAny<DeactivateUserCommand>(), default)).ReturnsAsync(user);
+            //mediator.Setup(m => m.Send(It.IsAny<DeactivateUserCommand>(), default)).ReturnsAsync(user);
 
             var result = await controller.SetActiveState(userId, command);
 
