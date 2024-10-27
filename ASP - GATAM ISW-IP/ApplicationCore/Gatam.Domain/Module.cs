3caed08af42ddad3999914a6b645d72706ae851c
s124285 <lauren.onya@student.ap.be>
2024-10-27 12:49:14 +0100

    feat: model aanmaken

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/Module.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Domain
+{
+    public class Module
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public required string Category { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+        public Module()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/Question.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatam.Domain
+{
+    public class Question
+    {
+        public string Id { get; set; }
+        public required string Category { get; set; }
+
+        public required string ModuleId { get; set; }
+        public Module Module { get; set; }
+
+        public QuestionType Type { get; set; }
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+        public bool AllowsMultipleAnswers { get; set; }
+        public List<string> CorrectAnswers { get; set; }
+
+        public Question()
+        {
+            Id = Guid.NewGuid().ToString();
+            Options = new List<string>();
+            CorrectAnswers = new List<string>();
+        }
+    }
+
+    public enum QuestionType
+    {
+        TrueFalse,       
+        MultipleChoice,  
+        OpenText,        
+        DropdownList     
+    }
+}
