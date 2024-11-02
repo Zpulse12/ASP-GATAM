fe8eeef0e7d1bb177bdb1603441e6641159750e5
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-11-02 21:18:51 +0100

    fix: set network bridge

--- Server setup/DeployProxySetup.sh
+++ Server setup/DeployProxySetup.sh
@@ -1,7 +1,7 @@
 if ! command -v "docker" >/dev/null 2>&1; then
     echo "docker not installed. Run SETUP script first."
 else
-    docker network create staging-proxy
-    docker network create production-proxy
+    docker network create --driver bridge staging-proxy
+    docker network create --driver bridge production-proxy
     docker compose --env-file ./.env -f ./docker-compose.yml up -d --build
 fi
