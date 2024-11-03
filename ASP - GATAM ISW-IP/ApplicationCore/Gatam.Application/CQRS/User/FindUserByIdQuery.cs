b9e65c5d9eb19fd892e7f4c61f46f6dd39809bc0
Haitam <haitam.baqloul@student.ap.be>
2024-11-03 22:57:51 +0100

    feat: add find user by id query

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/User/FindUserByIdQuery.cs
@@ -0,0 +1,30 @@
+﻿using Gatam.Application.Interfaces;
+using Gatam.Domain;
+using MediatR;
+
+namespace Gatam.Application.CQRS.User;
+
+public class FindUserByIdQuery : IRequest<ApplicationUser>
+{
+    public string Auth0UserId { get; set; }
+    
+    public FindUserByIdQuery(string auth0UserId)
+    {
+        Auth0UserId = auth0UserId;
+    }
+    public class FindUserByIdQueryHandler : IRequestHandler<FindUserByIdQuery, ApplicationUser>
+    {
+        private readonly IUnitOfWork _uow;
+
+        public FindUserByIdQueryHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<ApplicationUser> Handle(FindUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _uow.UserRepository.FindById(request.Auth0UserId);
+        }
+    }
+
+}
