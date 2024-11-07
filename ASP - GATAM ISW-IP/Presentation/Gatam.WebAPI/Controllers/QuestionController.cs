c4b419ff8334f2a575bc8b5f948d700a3146b6b9
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-11-07 17:39:21 +0100

    feat: add controller

---
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,27 @@
+﻿using Gatam.Application.CQRS.Questions;
+using Gatam.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gatam.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllQuestions()
+        {
+            return Ok("YES");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateQuestion([FromBody] Question question) 
+        {
+            Question createdQuestion = await _mediator.Send(new CreateQuestionCommand() { question = question});
+            return Ok(createdQuestion);
+        }
+    }
+}
