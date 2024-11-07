4f71a63bbb8fdfd38d2feacabe9a7dc0e9d7560d
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-11-07 17:38:57 +0100

    fix: add question entity + relation

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/Question.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/Question.cs
@@ -7,9 +7,29 @@ using System.Threading.Tasks;
 
 namespace Gatam.Domain
 {
+    public enum QuestionType
+    {
+        MULTIPLE_CHOICE,
+        OPEN,
+        TRUE_OR_FALSE,
+        CHOICE_LIST
+    }
     public class Question
     {
-     
+        public string Id { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public string QuestionTitle { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedUserId { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public string LastUpdatedUserId { get; set; }
+        public ICollection<ApplicationModule> ApplicationModules { get; set; } = new List<ApplicationModule>();
+        public string QuestionAnswer { get; set; }
+        public Question()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.UtcNow;
+            LastUpdatedAt = DateTime.UtcNow;
+        }
     }
-
 }
