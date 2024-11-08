b341e139de358a230e34dc7416f201574838f385
Haitam <haitam.baqloul@student.ap.be>
2024-11-08 16:21:44 +0100

    feat: rename property to the correct name id

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/AssignModulesToUserCommand.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/AssignModulesToUserCommand.cs
@@ -5,5 +5,5 @@ namespace Gatam.Application.CQRS.Module;
 public class AssignModulesToUserCommand : IRequest<UserDTO>
 {
     public string VolgerId { get; set; }
-    public string BegeleiderId { get; set; }
+    public string ModuleId { get; set; }
 }
