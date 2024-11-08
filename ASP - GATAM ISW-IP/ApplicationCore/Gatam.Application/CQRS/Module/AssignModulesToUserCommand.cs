d5d96e9c51157baa6edc26d13a225264ca1600ef
Haitam <haitam.baqloul@student.ap.be>
2024-11-08 16:18:29 +0100

    feat:base class to assign moduels to user

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/AssignModulesToUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Gatam.Application.CQRS.Module;
+
+public class AssignModulesToUserCommand : IRequest<UserDTO>
+{
+    public string VolgerId { get; set; }
+    public string BegeleiderId { get; set; }
+}
