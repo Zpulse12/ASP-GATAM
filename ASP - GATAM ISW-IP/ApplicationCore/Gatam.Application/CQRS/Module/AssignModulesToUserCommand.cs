3789828c553d0047f9e9bf35a6e01f49221f2f6d
Haitam <haitam.baqloul@student.ap.be>
2024-11-08 17:27:36 +0100

    feat: add empty handler for assingin modules to volgers

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/AssignModulesToUserCommand.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/AssignModulesToUserCommand.cs
@@ -7,3 +7,11 @@ public class AssignModulesToUserCommand : IRequest<UserDTO>
     public string VolgerId { get; set; }
     public string ModuleId { get; set; }
 }
+
+public class AssignModulesToUserCommandHandler : IRequestHandler<AssignModulesToUserCommand, UserDTO>
+{
+    public Task<UserDTO> Handle(AssignModulesToUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
