3af6536b2312741a4a1efe73f90aa3b804b5964e
Haitam <haitam.baqloul@student.ap.be>
2024-11-10 16:05:17 +0100

    feat: get users with modules and also get user with module query

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/GetUserModulesQuery.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Gatam.Application.Interfaces;
+using Gatam.Domain;
+using MediatR;
+
+namespace Gatam.Application.CQRS.User;
+
+public class GetUserModulesQuery: IRequest<List<ApplicationModule>>
+{
+    public string UserId { get; set; }
+
+    public GetUserModulesQuery(string userId)
+    {
+        UserId = userId;
+    }
+    public class GetUsersWithModulesQueryValidator : AbstractValidator<GetUsersWithModulesQuery>
+    {
+        public GetUsersWithModulesQueryValidator()
+        {
+            RuleFor(query => query)
+                .NotNull()
+                .WithMessage("De query mag niet null zijn.");
+        }
+    }
+    public class GetUserModulesQueryHandler : IRequestHandler<GetUserModulesQuery, List<ApplicationModule>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserModulesQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ApplicationModule>> Handle(GetUserModulesQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserWithModules(request.UserId);
+            if (user == null)
+                throw new KeyNotFoundException("User not found");
+
+            return _mapper.Map<List<ApplicationModule>>(user.UserModules.Select(um => um.Module).ToList());
+        }
+    }
+}
---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/Module/GetUsersWithModulesQuery.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using MediatR;
+using Gatam.Application.Interfaces;
+using Gatam.Application.CQRS.DTOS;
+
+namespace Gatam.Application.CQRS.User
+{
+    public class GetUsersWithModulesQuery : IRequest<List<UserDTO>> { }
+    public class GetUserModulesQueryValidator : AbstractValidator<GetUserModulesQuery>
+    {
+        public GetUserModulesQueryValidator()
+        {
+            RuleFor(query => query.UserId)
+                .NotEmpty()
+                .WithMessage("UserId is required.");
+        }
+    }
+    public class GetUsersWithModulesQueryHandler : IRequestHandler<GetUsersWithModulesQuery, List<UserDTO>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUsersWithModulesQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<UserDTO>> Handle(GetUsersWithModulesQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetUsersWithModulesAsync();
+            return users.Select(user => new UserDTO
+            {
+                Id = user.Id,
+                Nickname = user.UserName,
+                Email = user.Email,
+                Modules = user.UserModules.Select(um => um.Module.Title)
+                    .ToList(),
+                RolesIds = null
+            }).ToList();
+        }
+    }
+}
