a554847a9d59eb01370412771fad027bbd450705
Haitam <haitam.baqloul@student.ap.be>
2024-11-10 15:45:08 +0100

    feat: add 2 methods for to get users with their modules

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Gatam.Application.CQRS.DTOS;
+using Gatam.Domain;
+
+namespace Gatam.Application.Interfaces;
+
+public interface IUserRepository
+{
+    Task<ApplicationUser?> GetUserWithModules(string userId);
+    Task<List<ApplicationUser>> GetUsersWithModulesAsync();
+}
