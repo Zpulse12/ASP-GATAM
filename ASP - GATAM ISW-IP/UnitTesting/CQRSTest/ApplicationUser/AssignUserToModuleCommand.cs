ca9b2d1c8b6fef4c0b1660eba149fa091ae7b63f
Haitam <haitam.baqloul@student.ap.be>
2024-11-10 16:49:46 +0100

    Test: add test for the controller and the command for assigning modules to users

---
+++ ASP - GATAM ISW-IP/UnitTesting/CQRSTest/ApplicationUser/AssignUserToModuleCommand.cs
@@ -0,0 +1,75 @@
+﻿using Gatam.Application.CQRS.Module;
+using Gatam.Application.Interfaces;
+using Gatam.Domain;
+using Moq;
+
+namespace UnitTesting.CQRSTest.ApplicationUser;
+[TestClass]
+public class AssignUserToModuleCommand
+{
+ private Mock<IUnitOfWork> _mockUnitOfWork;
+        private AssignModulesToUserCommandHandler _handler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _handler = new AssignModulesToUserCommandHandler(_mockUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task Handle_ShouldAssignModuleToUser_WhenUserAndModuleExist()
+        {
+            var userId = "user-123";
+            var moduleId = "module-456";
+
+            var user = new Gatam.Domain.ApplicationUser()
+            {
+                Id = userId,
+                UserModules = new List<UserModule>()
+            };
+
+            var module = new ApplicationModule
+            {
+                Id = moduleId
+            };
+
+            _mockUnitOfWork.Setup(uow => uow.UserRepository.FindById(userId))
+                .ReturnsAsync(user);
+
+            _mockUnitOfWork.Setup(uow => uow.ModuleRepository.FindById(moduleId))
+                .ReturnsAsync(module);
+
+            var result = await _handler.Handle(new AssignModulesToUserCommand
+            {
+                VolgerId = userId,
+                ModuleId = moduleId
+            }, CancellationToken.None);
+
+            Assert.IsTrue(result);
+            _mockUnitOfWork.Verify(uow => uow.UserRepository.Update(It.Is<Gatam.Domain.ApplicationUser>(u => u.UserModules.Count == 1)), Times.Once);
+            _mockUnitOfWork.Verify(uow => uow.Commit(), Times.Once);
+        }
+        
+        [TestMethod]
+        public async Task Validator_ShouldReturnError_WhenUserDoesNotExist()
+        {
+            var command = new AssignModulesToUserCommand
+            {
+                VolgerId = "nonexistent-user",
+                ModuleId = "module-123"
+            };
+
+            var validator = new AssignModulesToUserCommandValidator(_mockUnitOfWork.Object);
+
+            _mockUnitOfWork.Setup(uow => uow.UserRepository.FindById(It.IsAny<string>()))
+                .ReturnsAsync((Gatam.Domain.ApplicationUser)null);
+
+            _mockUnitOfWork.Setup(uow => uow.ModuleRepository.FindById(It.IsAny<string>()))
+                .ReturnsAsync((ApplicationModule)null);
+            var validationResult = await validator.ValidateAsync(command);
+            Assert.IsFalse(validationResult.IsValid);
+            Assert.IsTrue(validationResult.Errors.Exists(e => e.ErrorMessage == "The user does not exist"));
+        } 
+}
+
---
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/GetUsersWithModulesController.cs
@@ -0,0 +1,78 @@
+﻿using Gatam.Application.CQRS;
+using Gatam.Application.CQRS.User;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace UnitTesting.ControllerTest.ApplicationUser;
+[TestClass]
+public class GetUsersWithModulesController
+{
+    private Mock<IMediator>? mediator;
+    private UserController? controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        mediator = new Mock<IMediator>();
+        controller = new UserController(mediator.Object);
+    }
+
+    [TestMethod]
+    public async Task GetUsersWithModules_ShouldReturnOk_WhenUsersExist()
+    {
+        var users = new List<UserDTO>
+        {
+            new UserDTO
+            {
+                Id = "2",
+                Nickname = "User2",
+                Email = "user2@example.com",
+                Modules = new List<string>
+                {
+                    "Module2"
+                },
+                RolesIds = null
+            }
+        };
+        users.Add(new UserDTO
+        {
+            Id = "1",
+            Nickname = "User1",
+            Email = "user1@example.com",
+            Modules = new List<string>
+            {
+                "Module1"
+            },
+            RolesIds = null
+        });
+
+        mediator.Setup(m => m.Send(It.IsAny<GetUsersWithModulesQuery>(), default))
+            .ReturnsAsync(users);
+
+        var result = await controller.GetUsersWithModules();
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+        Assert.AreEqual(users, okResult.Value);
+    }
+
+    [TestMethod]
+    public async Task GetUsersWithModules_ShouldReturnOk_WhenNoUsersExist()
+    {
+        mediator.Setup(m => m.Send(It.IsAny<GetUsersWithModulesQuery>(), default))
+            .ReturnsAsync(new List<UserDTO>());
+
+        var result = await controller.GetUsersWithModules();
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+        Assert.AreEqual(0, (okResult.Value as List<UserDTO>).Count);
+    }
+}
+
