9bf7dde3ddf5cfca601b803694db2a9b7e5c5d50
s124285 <lauren.onya@student.ap.be>
2024-11-11 17:49:12 +0100

    test: succesfull test

---
+++ ASP - GATAM ISW-IP/UnitTesting/CQRSTest/ApplicationUser/UnassignUserCommandTest.cs
@@ -0,0 +1,64 @@
+﻿using Gatam.Application.CQRS.User.BegeleiderAssignment;
+using Gatam.Domain;
+using Gatam.Application.Interfaces;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoMapper;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace UnitTesting.CQRSTest.ApplicationUser
+{
+   
+
+    [TestClass]
+    public class UnassignUserCommandTest
+    {
+        private Mock<IUnitOfWork> _uowMock;
+        private Mock<IMapper> _mapperMock;
+        private UnassignUserCommandHandler _handler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Maak mocks voor de afhankelijkheden
+            _uowMock = new Mock<IUnitOfWork>();
+            _mapperMock = new Mock<IMapper>();
+
+            // Maak de handler aan met de gemockte afhankelijkheden
+            _handler = new UnassignUserCommandHandler(_uowMock.Object, _mapperMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Handle_UserExists_UnassignsUserSuccessfully()
+        {
+            // Arrange
+            var volgerId = "volgerId";
+            var user = new Gatam.Domain.ApplicationUser
+            {
+                Id = volgerId,
+                BegeleiderId = "begeleiderId"  // Gebruiker heeft een begeleider
+            };
+
+            // Mock het gedrag van de UserRepository
+            _uowMock.Setup(u => u.UserRepository.FindById(volgerId)).ReturnsAsync(user);
+
+            // Act
+            var result = await _handler.Handle(new UnassignUserCommand { VolgerId = volgerId }, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(volgerId, result.Id);
+            Assert.IsNull(result.BegeleiderId); // BegeleiderId moet nu null zijn
+
+            // Verifieer of de Update methode werd aangeroepen
+            _uowMock.Verify(u => u.UserRepository.Update(It.Is<Gatam.Domain.ApplicationUser>(x => x.BegeleiderId == null)), Times.Once);
+
+            // Verifieer of commit werd aangeroepen
+            _uowMock.Verify(u => u.commit(), Times.Once);
+        }
+
+
+    }
+
+}
---
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/UnassignUsersToBegeleiderControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using Gatam.Application.CQRS;
+using Gatam.Application.CQRS.User.BegeleiderAssignment;
+using Gatam.Application.CQRS.User;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ControllerTest.ApplicationUser
+{
+    [TestClass]
+    public class UnassignUsersToBegeleiderControllerTest
+    {
+        private Mock<IMediator> _mediatorMock;
+        private UserController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new UserController(_mediatorMock.Object);
+        }
+
+        [TestMethod]
+        public async Task UnassignUsersToBegeleider_UserExists_ReturnsOkResult()
+        {
+            var userId = "volgerId";
+            var expectedResponse = true; // Verwacht een boolean resultaat (true)
+            var begeleiderId = "begeleiderId";
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<FindUserByIdQuery>(), default))
+                         .ReturnsAsync(new Gatam.Domain.ApplicationUser { Id = userId });
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<Gatam.Application.CQRS.User.BegeleiderAssignment.AssignUserToBegeleiderCommand>(), default))
+                      .ReturnsAsync(new Gatam.Domain.ApplicationUser { Id = "volgerId", BegeleiderId = begeleiderId });
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<UnassignUserCommand>(), default))
+                  .ReturnsAsync(new Gatam.Domain.ApplicationUser { Id = "volgerId" });
+
+            // Act
+            var result = await _controller.UnassignUsersToBegeleider(userId);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+
+        [TestMethod]
+        public async Task UnassignUsersToBegeleider_UserDoesNotExist_ReturnsNotFoundResult()
+        {
+            // Arrange
+            var userId = "nonExistentUserId";
+            _mediatorMock.Setup(m => m.Send(It.IsAny<FindUserByIdQuery>(), default))
+                .ReturnsAsync((Gatam.Domain.ApplicationUser)null);
+
+            // Act
+            var result = await _controller.UnassignUsersToBegeleider(userId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+    }
+}
