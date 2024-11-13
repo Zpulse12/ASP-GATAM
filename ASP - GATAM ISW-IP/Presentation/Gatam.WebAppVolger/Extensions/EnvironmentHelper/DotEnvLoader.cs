df292819f72d6154782366a23e7413bfbac63aad
Haitam <haitam.baqloul@student.ap.be>
2024-11-13 16:14:07 +0100

    feat: also copy layout and logic form the other frontend

---
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAppVolger/Extensions/EnvironmentHelper/DotEnvLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Gatam.WebAppBegeleider.Extensions.EnvironmentHelper
+{
+    public static class DotEnvLoader
+    {
+        public static void Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+
+        public static Match ValidateWithExpression(string expression, string text)
+        {
+            Regex reg = new Regex(expression, RegexOptions.IgnoreCase);
+            return reg.Match(text);
+        }
+
+    }
+}
---
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAppVolger/Extensions/EnvironmentHelper/EnvironmentWrapper.cs
@@ -0,0 +1,34 @@
+﻿using Gatam.Infrastructure.Exceptions;
+
+namespace Gatam.WebAppBegeleider.Extensions.EnvironmentHelper
+{
+    public class EnvironmentWrapper
+    {
+        public string AUTH0DOMAIN { get; set; }
+        public string AUTH0CLIENTID { get; set; }
+        public string AUTH0CLIENTSECRET { get; set; }
+        public string AUTH0AUDIENCE { get; set; }
+
+        public string ENVIRONMENT { get; set; }
+        public EnvironmentWrapper()
+        {
+#if DEBUG
+            DirectoryInfo rootDirectory = SolutionWrapper.GetSolutionDirectoryPath();
+            string dotenvPath = Path.Combine(rootDirectory.FullName, ".env");
+            DotEnvLoader.Load(dotenvPath);
+#endif
+            AUTH0DOMAIN = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? "";
+            AUTH0CLIENTID = Environment.GetEnvironmentVariable("AUTH0_CLIENTID") ?? "";
+            AUTH0CLIENTSECRET = Environment.GetEnvironmentVariable("AUTH0_CLIENTSECRET") ?? "";
+            AUTH0AUDIENCE = Environment.GetEnvironmentVariable("AUTH0_AUDIENCE") ?? "";
+            ENVIRONMENT = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "";
+
+            /// NULL CHECKS
+            if (string.IsNullOrEmpty(AUTH0DOMAIN)) { throw new MissingEnvironmentVariableException(nameof(AUTH0DOMAIN)); }
+            if (string.IsNullOrEmpty(AUTH0CLIENTID)) { throw new MissingEnvironmentVariableException(nameof(AUTH0CLIENTID)); }
+            if (string.IsNullOrEmpty(AUTH0CLIENTSECRET)) { throw new MissingEnvironmentVariableException(nameof(AUTH0CLIENTSECRET)); }
+            if (string.IsNullOrEmpty(AUTH0AUDIENCE)) { throw new MissingEnvironmentVariableException(nameof(AUTH0AUDIENCE)); }
+            if (string.IsNullOrEmpty(ENVIRONMENT)) { throw new MissingEnvironmentVariableException(nameof(ENVIRONMENT)); }
+        }
+    }
+}
