1ca47099af67bf8f24ef9a3dcb7d2b8b5a1792cc
Haitam <haitam.baqloul@student.ap.be>
2024-11-13 16:13:08 +0100

    feat: copy logic from begeleider to the new frontend of volger

---
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAppVolger/Extensions/Registrator.cs
@@ -0,0 +1,57 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Gatam.WebAppBegeleider.Extensions.EnvironmentHelper;
+
+namespace Gatam.WebAppVolger.Extensions
+{
+    public static class Registrator
+    {
+        public static IServiceCollection RegisterCustomApiClient(this IServiceCollection services) 
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            EnvironmentWrapper env = serviceProvider.GetRequiredService<EnvironmentWrapper>();
+            services.AddHttpContextAccessor();
+            services.AddScoped<TokenService>();
+            services.AddScoped<HeaderHandler>();
+
+            string _host = $"http://{env.ENVIRONMENT}-api:8080/";
+
+            services.AddHttpClient<ApiClient>((httpClient) =>
+            {
+                httpClient.BaseAddress = new Uri(_host);
+
+#if DEBUG
+                httpClient.BaseAddress = new Uri("http://localhost:5000");
+#endif
+            })
+            .AddHttpMessageHandler<HeaderHandler>();
+            return services;
+
+        }
+
+
+        public static IServiceCollection RegisterPolicies(this IServiceCollection services)
+        {
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireManagementRole", policy =>
+                {
+                    var requiredRoleIds = RoleMapper.GetRoleValues("BEHEERDER", "BEGELEIDER");
+                    policy.RequireRole(requiredRoleIds);
+
+                });
+                options.AddPolicy("RequireMakerRole", policy =>
+                {
+                    var requiredRoleIds = RoleMapper.GetRoleValues("BEHEERDER", "MAKER");
+                    policy.RequireRole(requiredRoleIds);
+                });
+                options.AddPolicy("RequireAdminRole", policy =>
+                {
+                    var requiredRoleIds = RoleMapper.GetRoleValues("BEHEERDER");
+                    policy.RequireRole(requiredRoleIds);
+                });
+            });
+            return services;
+        }
+    }
+}
