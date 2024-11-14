8286d48bd670a0e7b9299414559c74cea2639e45
s123318 <tony.makdesi@student.ap.be>
2024-11-14 12:37:33 +0100

    fix:Rewrite Create tests and fix other tests

--- ASP - GATAM ISW-IP/UnitTesting/CQRSTest/ApplicationUser/DeleteUserCommandTest.cs
+++ ASP - GATAM ISW-IP/UnitTesting/CQRSTest/ApplicationUser/DeleteUserCommandTest.cs
@@ -1,95 +1,95 @@
 ﻿
-//using FluentValidation;
-//using FluentValidation.Results;
-//using Gatam.Application.CQRS;
-//using Gatam.Application.CQRS.User;
-//using Gatam.Application.Interfaces;
-//using Moq;
+using FluentValidation;
+using FluentValidation.Results;
+using Gatam.Application.CQRS;
+using Gatam.Application.CQRS.User;
+using Gatam.Application.Interfaces;
+using Moq;
 
-//namespace UnitTesting.CQRSTest.ApplicationUser;
-//[TestClass]
-//public class DeleteUserCommandTest
-//{
-//    private Mock<IUnitOfWork> _unitOfWorkMock;
-//    private Mock<IGenericRepository<Gatam.Domain.ApplicationUser>> _userRepositoryMock;
-//    private Mock<IManagementApi> _managementApiMock;
-//    private DeleteUserCommandHandler _handler;
+namespace UnitTesting.CQRSTest.ApplicationUser;
+[TestClass]
+public class DeleteUserCommandTest
+{
+    private Mock<IUnitOfWork> _unitOfWorkMock;
+    private Mock<IGenericRepository<Gatam.Domain.ApplicationUser>> _userRepositoryMock;
+    private Mock<IManagementApi> _managementApiMock;
+    private DeleteUserCommandHandler _handler;
 
-//    [TestInitialize]
-//    public void Setup()
-//    {
-//        _managementApiMock = new Mock<IManagementApi>();
-//        _unitOfWorkMock = new Mock<IUnitOfWork>();
-//        _userRepositoryMock = new Mock<IGenericRepository<Gatam.Domain.ApplicationUser>>();
-        
-//        _unitOfWorkMock.Setup(mock => mock.UserRepository).Returns(_userRepositoryMock.Object);
-//        _handler = new DeleteUserCommandHandler(_userRepositoryMock.Object, _managementApiMock.Object);
-//    }
+    [TestInitialize]
+    public void Setup()
+    {
+        _managementApiMock = new Mock<IManagementApi>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _userRepositoryMock = new Mock<IGenericRepository<Gatam.Domain.ApplicationUser>>();
 
-//    [TestMethod]
-//    public async Task DeleteUser_UserNotFound_ShouldReturnFalse()
-//    {
-//        const string userId = "nonexistentUser";
-//        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync((Gatam.Domain.ApplicationUser)null);
-//        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync((UserDTO)null);
-        
-//        var command = new DeleteUserCommand { UserId = userId };
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        _unitOfWorkMock.Setup(mock => mock.UserRepository).Returns(_userRepositoryMock.Object);
+        _handler = new DeleteUserCommandHandler(_userRepositoryMock.Object, _managementApiMock.Object);
+    }
 
-//        Assert.IsFalse(result);
-//        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
-//    }
+    [TestMethod]
+    public async Task DeleteUser_UserNotFound_ShouldReturnFalse()
+    {
+        const string userId = "nonexistentUser";
+        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync((Gatam.Domain.ApplicationUser)null);
+        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync((UserDTO)null);
 
-//    [TestMethod]
-//    public async Task DeleteUser_UserExists_ShouldReturnTrue()
-//    {
-//        const string userId = "user123";
-//        var user = new Gatam.Domain.ApplicationUser { Id = userId };
-//        var userDto = new UserDTO
-//        {
-//            Id = userId,
-//            Name = null,
-//            Surname= null,
-//            PhoneNumber= null,
-//            Email = null,
-//            RolesIds = null
-//        }; 
+        var command = new DeleteUserCommand { UserId = userId };
+        var result = await _handler.Handle(command, CancellationToken.None);
 
-//        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync(user);
-//        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync(userDto);
-//        _managementApiMock.Setup(api => api.DeleteUserAsync(userId)).ReturnsAsync(true);
-        
-//        var command = new DeleteUserCommand { UserId = userId };
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        Assert.IsFalse(result);
+        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
+    }
 
-//        Assert.IsTrue(result);
-//        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
-//        _userRepositoryMock.Verify(repo => repo.Delete(user), Times.Once);
-//    }
+    [TestMethod]
+    public async Task DeleteUser_UserExists_ShouldReturnTrue()
+    {
+        const string userId = "user123";
+        var user = new Gatam.Domain.ApplicationUser { Id = userId };
+        var userDto = new UserDTO
+        {
+            Id = userId,
+            Name = null,
+            Surname= null,
+            PhoneNumber= null,
+            Email = null,
+            RolesIds = null
+        };
 
-//    [TestMethod]
-//    public async Task DeleteUser_Auth0DeleteFails_ShouldReturnFalse()
-//    {
-//        const string userId = "user123";
-//        var user = new Gatam.Domain.ApplicationUser { Id = userId };
-//        var userDto = new UserDTO
-//        {
-//            Id = userId,
-//            Name = null,
-//            Surname=null,
-//            PhoneNumber= null,
-//            Email = null,
-//            RolesIds = null
-//        }; 
+        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync(user);
+        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync(userDto);
+        _managementApiMock.Setup(api => api.DeleteUserAsync(userId)).ReturnsAsync(true);
 
-//        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync(user);
-//        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync(userDto);
-//        _managementApiMock.Setup(api => api.DeleteUserAsync(userId)).ReturnsAsync(false);
+        var command = new DeleteUserCommand { UserId = userId };
+        var result = await _handler.Handle(command, CancellationToken.None);
 
-//        var command = new DeleteUserCommand { UserId = userId };
-//        var result = await _handler.Handle(command, CancellationToken.None);
+        Assert.IsTrue(result);
+        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
+        _userRepositoryMock.Verify(repo => repo.Delete(user), Times.Once);
+    }
 
-//        Assert.IsFalse(result);
-//        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
-//    }
-//}
+    [TestMethod]
+    public async Task DeleteUser_Auth0DeleteFails_ShouldReturnFalse()
+    {
+        const string userId = "user123";
+        var user = new Gatam.Domain.ApplicationUser { Id = userId };
+        var userDto = new UserDTO
+        {
+            Id = userId,
+            Name = null,
+            Surname=null,
+            PhoneNumber= null,
+            Email = null,
+            RolesIds = null
+        };
+
+        _userRepositoryMock.Setup(repo => repo.FindById(userId)).ReturnsAsync(user);
+        _managementApiMock.Setup(api => api.GetUserByIdAsync(userId)).ReturnsAsync(userDto);
+        _managementApiMock.Setup(api => api.DeleteUserAsync(userId)).ReturnsAsync(false);
+
+        var command = new DeleteUserCommand { UserId = userId };
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        Assert.IsFalse(result);
+        _userRepositoryMock.Verify(repo => repo.FindById(userId), Times.Once);
+    }
+}
--- ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/CreateUserControllerTest.cs
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/CreateUserControllerTest.cs
@@ -1,56 +1,54 @@
-﻿//using Gatam.Application.CQRS;
-//using Gatam.WebAPI.Controllers;
-//using MediatR;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using Moq;
-//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Gatam.Application.CQRS.User;
+﻿using Gatam.Application.CQRS;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gatam.Application.CQRS.User;
 
-//namespace UnitTesting.ControllerTest.ApplicationUser
-//{
-//    [TestClass]
-//    public class CreateUserControllerTest
-//    {
-//        private Mock<IMediator> _mediatorMock;
-//        private UserController _userController;
-//        private IPasswordHasher<Gatam.Domain.ApplicationUser> _passwordHasher;
+namespace UnitTesting.ControllerTest.ApplicationUser
+{
+    [TestClass]
+    public class CreateUserControllerTest
+    {
+        private Mock<IMediator> _mediatorMock;
+        private UserController _userController;
+        private IPasswordHasher<Gatam.Domain.ApplicationUser> _passwordHasher;
 
-//        [TestInitialize]
-//        public void Setup()
-//        {
-//            _mediatorMock = new Mock<IMediator>();
-//            _userController = new UserController( _mediatorMock.Object );
-//            _passwordHasher = new PasswordHasher<Gatam.Domain.ApplicationUser>();
-//        }
-//        [TestMethod]
-//        public async Task CreateUser_ReturnsCreated_AndUser()
-//        {
-//            Gatam.Domain.ApplicationUser user = new Gatam.Domain.ApplicationUser() { Name="Test", Email="test@email.com" };
-//            user.PasswordHash = _passwordHasher.HashPassword(user, "test1234!@");
+        [TestInitialize]
+        public void Setup()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _userController = new UserController(_mediatorMock.Object);
+            _passwordHasher = new PasswordHasher<Gatam.Domain.ApplicationUser>();
+        }
+        [TestMethod]
+        public async Task CreateUser_ReturnsCreated_AndUser()
+        {
+            UserDTO user = new UserDTO() { Name="Test", Email="test@email.com" };
 
-//            _mediatorMock.Setup(setup => setup.Send(It.IsAny<CreateUserCommand>(), default)).ReturnsAsync(user);
+            _mediatorMock.Setup(setup => setup.Send(It.IsAny<CreateUserCommand>(), default)).ReturnsAsync(user);
 
-//            IActionResult result = await _userController.CreateUser(user);
+            IActionResult result = await _userController.CreateUser(user);
 
-//            CreatedResult? createdResult = result as CreatedResult;
-//            Assert.IsNotNull(createdResult);
-//            Assert.AreEqual(201, createdResult.StatusCode);
-//            Assert.AreEqual(user, createdResult.Value);
-//        }
-//        [TestMethod]
-//        public async Task CreateUser_ReturnsBadRequest_EmptyMail()
-//        {
-//            _userController.ModelState.AddModelError("Email", "Email cannot be empty");
-//            Gatam.Domain.ApplicationUser user = new Gatam.Domain.ApplicationUser() { Name = "Test", Email = "" };
-//            user.PasswordHash = _passwordHasher.HashPassword(user, "test1234!@");
-//            IActionResult result = await _userController.CreateUser(user);
-//            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
-//        }
-//    }
-//}
+            CreatedResult? createdResult = result as CreatedResult;
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(201, createdResult.StatusCode);
+            Assert.AreEqual(user, createdResult.Value);
+        }
+        [TestMethod]
+        public async Task CreateUser_ReturnsBadRequest_EmptyMail()
+        {
+            _userController.ModelState.AddModelError("Email", "Email cannot be empty");
+            UserDTO user = new UserDTO { Name = "Test", Email = "" };
+            IActionResult result = await _userController.CreateUser(user);
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+    }
+}
