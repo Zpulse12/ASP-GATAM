342abdde22a980ea49e9bfb5364c80b92f90fbb9
Haitam <haitam.baqloul@student.ap.be>
2024-11-14 19:47:13 +0100

    Test: add test for controller and query getting begeleider for the assigned user

---
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationUser/GetBegeleiderForUserTest.cs
@@ -0,0 +1,39 @@
+﻿using Gatam.Application.CQRS;
+using Gatam.Application.CQRS.User;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace UnitTesting.ControllerTest.ApplicationUser;
+[TestClass]
+public class GetBegeleiderForUserTest
+{
+    private Mock<IMediator> _mockMediator;
+    private UserController _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockMediator = new Mock<IMediator>();
+        _controller = new UserController(_mockMediator.Object);
+    }
+
+    [TestMethod]
+    public async Task GetBegeleiderForUser_ShouldReturnOk_WhenBegeleiderExists()
+    {
+        var userDto = new UserDTO
+        {
+            Id = "begeleider123",
+            Nickname = "BegeleiderNaam",
+            Email = null,
+            RolesIds = null
+        };
+        _mockMediator.Setup(m => m.Send(It.IsAny<GetBegeleiderForUserQuery>(), default))
+            .ReturnsAsync(userDto);
+        var result = await _controller.GetBegeleiderForUser("user123") as OkObjectResult;
+        Assert.IsNotNull(result);
+        Assert.AreEqual(200, result.StatusCode);
+        Assert.IsInstanceOfType(result.Value, typeof(UserDTO));
+    }
+}
