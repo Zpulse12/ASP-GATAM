6fe5de445ce403badd6f4de254e8316887cc0fa5
Haitam <haitam.baqloul@student.ap.be>
2024-11-21 21:55:38 +0100

    feat: add class for the relation

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/UserModuleQuestionSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Gatam.Domain;
+
+public class UserModuleQuestionSetting
+{
+    public string Id { get; set; }
+
+    public string UserModuleId { get; set; }
+    public virtual UserModule UserModule { get; set; }
+
+    public string QuestionId { get; set; }
+    public virtual Question Question { get; set; }
+    public bool IsVisible { get; set; }
+
+    public UserModuleQuestionSetting()
+    {
+        Id = Guid.NewGuid().ToString();
+        IsVisible = true;
+    }
+}
