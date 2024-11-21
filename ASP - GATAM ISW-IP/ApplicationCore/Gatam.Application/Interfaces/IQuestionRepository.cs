9aa9debfeec778412f9a00261cdaf1be5c5e15e5
Haitam <haitam.baqloul@student.ap.be>
2024-11-21 22:02:13 +0100

    add the methods to find aND UPDATE the data

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IQuestionRepository.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IQuestionRepository.cs
@@ -6,4 +6,7 @@ public interface IQuestionRepository : IGenericRepository<Question>
 {
     Task<Question> GetQuestionAndAnswers(string Id);
     Task UpdateQuestionAndAnswers(Question entity);
+    Task<List<Question>> GetVisibleQuestionsForVolger(string volgerId);
+    Task<UserModuleQuestionSetting> GetQuestionSettingById(string id);
+    Task UpdateQuestionSetting(UserModuleQuestionSetting setting);
 }
