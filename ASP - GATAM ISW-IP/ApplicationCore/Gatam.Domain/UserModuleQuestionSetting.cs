32cb22991e3dd13ba18f0f1e10da511a8f0249ee
Haitam <haitam.baqloul@student.ap.be>
2024-11-22 00:39:46 +0100

    fix: remove useless virtual

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/UserModuleQuestionSetting.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Domain/UserModuleQuestionSetting.cs
@@ -7,10 +7,10 @@ public class UserModuleQuestionSetting
     public string Id { get; set; }
 
     public string UserModuleId { get; set; }
-    public virtual UserModule UserModule { get; set; }
+    public UserModule UserModule { get; set; }
 
     public string QuestionId { get; set; }
-    public virtual Question Question { get; set; }
+    public Question Question { get; set; }
     public bool IsVisible { get; set; }
 
     public UserModuleQuestionSetting()
