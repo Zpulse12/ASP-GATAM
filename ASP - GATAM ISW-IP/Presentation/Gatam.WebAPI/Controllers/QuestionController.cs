24e008ff5b0705a872571a5613b2ea3c059a9e34
Haitam <haitam.baqloul@student.ap.be>
2024-11-21 22:08:12 +0100

    feat: add the endpoints for the question setting

--- ASP - GATAM ISW-IP/Presentation/Gatam.WebAPI/Controllers/QuestionController.cs
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAPI/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
-﻿using Gatam.Application.CQRS.Questions;
+﻿using ApplicationCore.CQRS.Questions;
+using Gatam.Application.CQRS.Questions;
 using Gatam.Application.CQRS.User;
 using Gatam.Domain;
 using MediatR;
@@ -27,12 +28,28 @@ namespace Gatam.WebAPI.Controllers
             return Ok(questions);
         }
 
+        [HttpPost("settings")]
+        [Authorize(Policy = "RequireManagementRole")]
+
+        public async Task<IActionResult> CreateSetting([FromBody] CreateSettingCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
         [HttpPost]
         [Authorize(Policy = "RequireMakerRole")]
         public async Task<IActionResult> CreateQuestion([FromBody] Question question) 
         {
             Question createdQuestion = await _mediator.Send(new CreateQuestionCommand() { question = question});
             return Created("", createdQuestion);
+        }
+        [HttpGet("visible/{volgerId}")]
+        [Authorize(Policy = "RequireManagementRole")]
+        public async Task<IActionResult> GetVisibleQuestionsForVolger(string volgerId)
+        {
+            var questions = await _mediator.Send(new GetVisibleQuestionsForVolgerQuery { VolgerId = volgerId });
+            return Ok(questions);
         }
 
         [HttpGet("{questionId}")]
@@ -54,6 +71,23 @@ namespace Gatam.WebAPI.Controllers
         {
             var returnedQuestion = await _mediator.Send(new UpdateQuestionCommand() {  Question = question, Id = questionId, });
             return Ok(returnedQuestion);
+        }
+
+        // [HttpGet("settings/usermodule/{userModuleId}")]
+        // public async Task<IActionResult> GetQuestionSettingsForUserModule(string userModuleId)
+        // {
+        //     var settings = await _mediator.Send(new GetQuestionSettingsForUserModuleQuery { UserModuleId = userModuleId });
+        //     return Ok(settings);
+        // }
+
+        [HttpPut("visibility")]
+        [Authorize(Policy = "RequireManagementRole")]
+
+        public async Task<IActionResult> UpdateVisibility([FromBody] UpdateQuestionVisibilityCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result) return NotFound();
+            return Ok();
         }
     }
 }
