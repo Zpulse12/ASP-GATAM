e4d394321dc245273e8379eb58adf65b54772d7a
Haitam <haitam.baqloul@student.ap.be>
2024-11-21 22:02:30 +0100

    feat: add the interface repo for module

---
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IModuleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Gatam.Domain;
+
+namespace Gatam.Application.Interfaces;
+
+public interface IModuleRepository:IGenericRepository<ApplicationModule>
+{
+     Task<ApplicationModule> FindByIdWithQuestions(string moduleId);
+
+}
