67967e07117fa4144ccfff5d83aa5f05bd24fb49
Haitam <haitam.baqloul@student.ap.be>
2024-11-21 22:01:07 +0100

    feaT: find a module with its question

--- ASP - GATAM ISW-IP/Infrastructure/Gatam.Infrastructure/Repositories/ModuleRepository.cs
+++ ASP - GATAM ISW-IP/Infrastructure/Gatam.Infrastructure/Repositories/ModuleRepository.cs
@@ -1,14 +1,23 @@
-﻿using Gatam.Domain;
+﻿using Gatam.Application.Interfaces;
+using Gatam.Domain;
 using Gatam.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gatam.Infrastructure.Repositories
 {
-    public class ModuleRepository : GenericRepository<ApplicationModule>
+    public class ModuleRepository : GenericRepository<ApplicationModule>, IModuleRepository
     {
         private readonly ApplicationDbContext _context;
 
         public ModuleRepository(ApplicationDbContext context) : base(context) {
             _context = context;
+        }
+
+        public async Task<ApplicationModule> FindByIdWithQuestions(string moduleId)
+        {
+            return await _context.Set<ApplicationModule>()
+                .Include(m => m.Questions) // Haal de vragen op
+                .FirstOrDefaultAsync(m => m.Id == moduleId);
         }
     }
 }
