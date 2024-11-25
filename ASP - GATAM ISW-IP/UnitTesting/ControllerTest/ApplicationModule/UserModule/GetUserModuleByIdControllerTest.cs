3d0f9fa41400b20b29cc3bbb4ffae7e698384dd7
s124285 <lauren.onya@student.ap.be>
2024-11-25 13:09:10 +0100

    test: succesfull test

---
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationModule/UserModule/GetUserModuleByIdControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using Gatam.Application.CQRS.DTOS.ModulesDTO;
+using Gatam.Application.CQRS.Module.UserModules;
+using Gatam.Domain;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ControllerTest.ApplicationModule.UserModule
+{
+    [TestClass]
+    public class GetUserModuleByIdControllerTest
+    {
+        private Mock<IMediator> _mediatorMock;
+        private UserModuleController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mediatorMock = new Mock<IMediator>();
+           
+
+            _controller = new UserModuleController(_mediatorMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetUserModules_ReturnsOkResult_WithModules()
+        {
+            // Arrange
+            var userId = "user1";
+            var modules = new List<UserModuleDTO>
+            {
+                new UserModuleDTO { Id = "module1" },
+                new UserModuleDTO { Id = "module2" }
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<GetUserModuleByUserQuery>(), default))
+                .ReturnsAsync(modules);
+
+            // Act
+            var result = await _controller.GetUserModules(userId);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+
+            var returnedModules = okResult.Value as List<UserModuleDTO>;
+            Assert.IsNotNull(returnedModules);
+            Assert.AreEqual(modules.Count, returnedModules.Count);
+            Assert.AreEqual(modules[0].Id, returnedModules[0].Id);
+            Assert.AreEqual(modules[1].Id, returnedModules[1].Id);
+
+            // Verify that the correct query was sent
+            _mediatorMock.Verify(m => m.Send(It.IsAny<GetUserModuleByUserQuery>(), default), Times.Once);
+        }
+
+    }
+}
---
+++ ASP - GATAM ISW-IP/UnitTesting/ControllerTest/ApplicationModule/UserModule/GetUserModulesControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Gatam.Application.CQRS.DTOS.ModulesDTO;
+using Gatam.Application.CQRS.Module.UserModules;
+using Gatam.Domain;
+using Gatam.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ControllerTest.ApplicationModule.UserModule
+{
+    [TestClass]
+    public class GetUserModulesControllerTest
+    {
+        private Mock<IMediator> _mediatorMock;
+        private UserModuleController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new UserModuleController(_mediatorMock.Object);
+        }
+
+        [TestMethod]
+        public async Task SubmitAnswers_ReturnsOkResult_WithUpdatedUserAnswers()
+        {
+            var userModuleId = "module1";
+            var userAnswers = new List<UserAnswer>
+    {
+        new UserAnswer { Id = "answer1", GivenAnswer = "TestAnswer" }
+    };
+            var updatedAnswers = userAnswers;
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<UpdateUserModuleStatusCommand>(), default))
+                .ReturnsAsync(new Gatam.Domain.UserModule
+                {
+                    Id = userModuleId,
+                    State = UserModuleState.InProgress
+                });
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<SubmitUserAnswersCommand>(), default))
+                .ReturnsAsync(updatedAnswers);
+
+            var result = await _controller.SubmitAnswers(userModuleId, userAnswers);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+
+            var returnedAnswers = okResult.Value as List<UserAnswer>;
+            Assert.IsNotNull(returnedAnswers);
+            Assert.AreEqual(userAnswers.Count, returnedAnswers.Count);
+
+            _mediatorMock.Verify(m => m.Send(It.IsAny<UpdateUserModuleStatusCommand>(), default), Times.Once);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<SubmitUserAnswersCommand>(), default), Times.Once);
+        }
+
+    }
+}
