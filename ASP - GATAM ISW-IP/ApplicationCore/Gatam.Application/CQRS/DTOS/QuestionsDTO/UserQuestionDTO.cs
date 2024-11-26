e490ee4ecdf9d7b541de04bf93883938b561e0cc
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-11-26 23:24:03 +0100

    feat: backend command + entity

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/DTOS/QuestionsDTO/UserQuestionDTO.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/DTOS/QuestionsDTO/UserQuestionDTO.cs
@@ -7,6 +7,7 @@ public class UserQuestionDTO
     public string UserModuleId { get; set; }
     public string Id { get; set; }
     public bool IsVisible { get; set; }
+    public QuestionPriority QuestionPriority { get; set; }
     public string QuestionId { get; set; }
     public string QuestionTitle { get; set; }
     public int QuestionType { get; set; }
