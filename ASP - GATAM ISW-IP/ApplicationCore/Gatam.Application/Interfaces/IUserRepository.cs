7598571d1ca28ad44ea1b8521d62015e4a4d9e47
Haitam <haitam.baqloul@student.ap.be>
2024-11-27 22:06:36 +0100

    fix: add new methods to the user repo

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IUserRepository.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/Interfaces/IUserRepository.cs
@@ -7,5 +7,8 @@ public interface IUserRepository : IGenericRepository<ApplicationUser>
     Task<ApplicationUser?> GetUserWithModules(string userId);
     Task<List<ApplicationUser>> GetUsersWithModulesAsync();
     Task<List<ApplicationUser>> GetUsersForBegeleiderAsync(string begeleiderId);
+    void RemoveUserRole(UserRole userRole);
+    Task<ApplicationUser?> GetUserWithRoles(string userId);
+
 
 }
