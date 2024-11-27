41b32d5c51b6fcd9fb5ee6efce03b07345ae77c5
Haitam <haitam.baqloul@student.ap.be>
2024-11-27 22:05:09 +0100

    fix: add the correct roles and also delete the user with the userrole

--- ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/User/DeleteUserCommand.cs
+++ ASP - GATAM ISW-IP/ApplicationCore/Gatam.Application/CQRS/User/DeleteUserCommand.cs
@@ -34,19 +34,44 @@ namespace Gatam.Application.CQRS.User
                 .WithMessage("The user doesnt exist");
         }
     }
-    public class DeleteUserCommandHandler(IUserRepository userRepository, IManagementApi managementApi)
-        : IRequestHandler<DeleteUserCommand, bool>
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IManagementApi _managementApi;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteUserCommandHandler(
+            IUserRepository userRepository, 
+            IManagementApi managementApi,
+            IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _managementApi = managementApi;
+            _unitOfWork = unitOfWork;
+        }
+
         public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
         {
-            var user = await userRepository.FindById(request.UserId);
-            var result = await managementApi.DeleteUserAsync(request.UserId);
-            if (result)
+            var user = await _userRepository.FindById(request.UserId);
+            if (user == null)
             {
-                await userRepository.Delete(user);
-                return true;
+                return false;
             }
-            return false;
+            var auth0Result = await _managementApi.DeleteUserAsync(request.UserId);
+            if (!auth0Result)
+            {
+                return false;
+            }
+
+            if (user.UserRoles != null)
+            {
+                user.UserRoles.Clear();
+            }
+
+            await _userRepository.Delete(user);
+            await _unitOfWork.Commit();
+
+            return true;
         }
     }
 }
