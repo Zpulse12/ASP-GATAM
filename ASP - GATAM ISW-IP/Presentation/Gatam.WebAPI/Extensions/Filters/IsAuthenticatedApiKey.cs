0542e022f3540d6805b0efb6f31f9f71c471db32
s112753 <nils.vanvaerenbergh@student.ap.be>
2024-12-13 16:48:08 +0100

    refactor: remove comments

--- ASP - GATAM ISW-IP/Presentation/Gatam.WebAPI/Extensions/Filters/IsAuthenticatedApiKey.cs
+++ ASP - GATAM ISW-IP/Presentation/Gatam.WebAPI/Extensions/Filters/IsAuthenticatedApiKey.cs
@@ -12,7 +12,6 @@ namespace Gatam.WebAPI.Extensions.Filters
         {
             _environmentWrapper = environmentWrapper;
         }
-        // (Nils) voor actie aangeroepen wordt
         public void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.HttpContext.Request.Headers.TryGetValue("X-Api-Key", out var apiKey) || string.IsNullOrEmpty(apiKey))
@@ -27,7 +26,6 @@ namespace Gatam.WebAPI.Extensions.Filters
                 return;
             }
         }
-        // (Nils) na actie aangeroepen werd (Geen filter voor nodig...)
         public void OnActionExecuted(ActionExecutedContext context) { }
     }
 
